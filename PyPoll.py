b24602356f51ef47da06d63013a99a7278fbf962
Danielshi1996 <79021858+Danielshi1996@users.noreply.github.com>
2021-07-19 00:01:39 -0400

    Challenge assignment

--- PyPoll.py
+++ PyPoll.py
@@ -1,45 +1,76 @@
 import datetime as dt
 import csv
 import os
-# Create a filename variable to a direct or indirect path to the file.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Create a filename variable to a direct or indirect path to the file.
+# Assign a variable to save the file to a path.
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
+total_vote = 0
+
+candidate_option = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
+    # Read the header row.
     header = next(file_reader)
 
-    print(header)
-    # for row in file_reader:
-    #     print(row)
+    # print each row
+    for row in file_reader:
+        total_vote += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_option:
+            candidate_option.append(candidate_name)
 
+            candidate_votes[candidate_name] = 0
 
-#     reader = csv.reader(election_data)
+        candidate_votes[candidate_name] += 1
 
-#     header = next(reader)
+# encoding = "utf-8" to prevent error in the code
 
-#     print(header)
+with open(file_to_save, "w", encoding="utf-8") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_vote:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
 
-#     candidates = []
+    txt_file.write(election_results)
 
-#     total = 0
+    for candidate_name in candidate_votes:
 
-#     for row in reader:
+        votes = candidate_votes[candidate_name]
 
-#         total += 1
-#         if row[2] not in candidates:
+        vote_percentage = float(votes)/float(total_vote) * 100
 
-#             candidates.append(row[2])
+        candidate_results = (
+            f'{candidate_name}：{vote_percentage:.1f}% ({votes:,})\n')
 
-#     print(candidates)
-#     print(total)
+        print(candidate_results)
 
+        txt_file.write(candidate_results)
 
-# # Using the open() function with the "w" mode we will write data to the file.
-# with open(file_to_save, "w") as outfile:
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
 
-#     outfile.write("Arapahoe,")
-#     outfile.write("Denver,")
-#     outfile.write("Jefferson")
+            winning_count = votes
+            winning_percentage = vote_percentage
+
+            winning_candidate = candidate_name
+
+    Winning_candidate_summary = (
+        f'---------------------------\n'
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count}\n"
+        f"Winning percentage: {winning_percentage:.1f}%\n"
+        f"---------------------------\n")
+    print(Winning_candidate_summary)
+
+    txt_file.write(Winning_candidate_summary)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Election_Analysis
+
+## Purpose
+
+The purpose of this analysis is to determine the result of the election, as well as the ballot distribution to candidates and the distribution of ball
